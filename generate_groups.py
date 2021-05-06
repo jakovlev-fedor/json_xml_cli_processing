468296d28d77a905f0b5087d0a28094065a98987
jakovlev-fedor <jakovlev.fedor.me@gmail.com>
2021-05-06 20:43:45 +0300

    Core functionality implemented:
    - to read 2 files from cwd
    - to generate new file with information combined from both files
    - to save new file in json format

--- generate_groups.py
+++ generate_groups.py
@@ -1,13 +1,33 @@
+import json
 
-# 1 TODO: open required files
+with open('rooms.json', 'r') as file:
+    rooms_json_data = json.load(file)
 
-# 2 TODO: load data from files
+with open('students.json', 'r') as file:
+    students_json_data = json.load(file)
 
-# 3 TODO: generate new data
 
-# 4 TODO: dump data to file
+groups_json_data = rooms_json_data
+
+
+for i, room in enumerate(groups_json_data):
+    groups_json_data[i]['students'] = []
+
+
+for student in students_json_data:
+    room_id = student['room']
+    room = groups_json_data[room_id]
+    room['students'].append(student)
+    groups_json_data[room_id] = room
+
+
+with open('groups.json', 'w') as write_file:
+    json.dump(groups_json_data, write_file, indent=4)
+
+
 
 # 5 TODO: implement different output formats
 
+# 5.1 TODO: Implement json to xml conversion
 
 # 6 TODO: implement cli integration support
