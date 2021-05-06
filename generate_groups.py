798c05d5808e20123249f80333aec5f8518ed0e2
jakovlev-fedor <jakovlev.fedor.me@gmail.com>
2021-05-06 18:35:23 +0300

    initial commit

---
+++ generate_groups.py
@@ -0,0 +1,12 @@
+
+# 1 TODO: open required files
+
+# 2 TODO: load data from files
+
+# 3 TODO: generate new data
+
+# 4 TODO: dump data to file
+
+# 5 TODO: implement different output formats
+
+# 6 TODO: implement cli integration support
