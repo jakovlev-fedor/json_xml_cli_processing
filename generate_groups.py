7dc9b1912607a2e5c0ecd079e4b28d326fccfb8e
jakovlev-fedor <jakovlev.fedor.me@gmail.com>
2021-05-09 12:39:24 +0300

    XML conversion and output implemented
    - xmltodict external library have been used
    - requirements.txt file created
    - .gitignore file was updated to exclude generated files

--- generate_groups.py
+++ generate_groups.py
@@ -1,33 +1,45 @@
 import json
+import xmltodict
 
+# --------------- OPEN REQUIRED FILES ---------------
 with open('rooms.json', 'r') as file:
-    rooms_json_data = json.load(file)
+    rooms_list = json.load(file)
 
 with open('students.json', 'r') as file:
-    students_json_data = json.load(file)
+    students_list = json.load(file)
 
 
-groups_json_data = rooms_json_data
+# --------------- GENERATE NEW JSON ---------------
+groups_list = rooms_list
 
+for i, room in enumerate(groups_list):
+    groups_list[i]['student'] = []
 
-for i, room in enumerate(groups_json_data):
-    groups_json_data[i]['students'] = []
-
-
-for student in students_json_data:
+for student in students_list:
     room_id = student['room']
-    room = groups_json_data[room_id]
-    room['students'].append(student)
-    groups_json_data[room_id] = room
+    room = groups_list[room_id]
+    room['student'].append(student)
+    groups_list[room_id] = room
 
 
+# --------------- ADJUST NEW JSON FOR XML CONVERSION ---------------
+rooms_dict = {}
+for i, room in enumerate(groups_list):
+    room_name = f'Room_{i}'
+    rooms_dict[room_name] = groups_list[i]
+single_root_dict = {'root': rooms_dict}
+groups_xml_data = xmltodict.unparse(single_root_dict, pretty=True)
+
+
+# --------------- WRITE NEW JSON ---------------
 with open('groups.json', 'w') as write_file:
-    json.dump(groups_json_data, write_file, indent=4)
+    json.dump(groups_list, write_file, indent=4)
 
+# --------------- WRITE TO NEW XML ---------------
+with open('groups.xml', 'w') as file:
+    file.write(groups_xml_data)
 
-
-# 5 TODO: implement different output formats
-
-# 5.1 TODO: Implement json to xml conversion
 
 # 6 TODO: implement cli integration support
+
+# 7 TODO: refactor code
